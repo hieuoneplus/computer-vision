e06612e615ddefeb86e33361c6222d0d92bcd287
hhhoanganh <101188848+hhhoanganh@users.noreply.github.com>
2023-12-13 10:46:48 +0700

    Update rotate.py

--- rotate.py
+++ rotate.py
@@ -1,17 +1,15 @@
 from imutils.perspective import four_point_transform
 import cv2
-import numpy as np
+import numpy
 
-def rotate(image):
-    # Load image, grayscale, Gaussian blur, Otsu's threshold
+# Load image, grayscale, Gaussian blur, Otsu's threshold
+def rotation(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (9,9), 0)
-    thresh = cv2.adaptiveThreshold(
-        blur, 220, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 3)
+    blur = cv2.GaussianBlur(gray, (7,7), 0)
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-    # Find contours and sort for largest contour
+# Find contours and sort for largest contour
     cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
     displayCnt = None
@@ -23,11 +21,13 @@ def rotate(image):
         if len(approx) == 4:
             displayCnt = approx
             break
-    # print(displayCnt)
-    if displayCnt is not None:
-        # Obtain birds' eye view of image
-        warped = four_point_transform(image, displayCnt.reshape(4, 2))
-        return warped
-    else:
-        # Return original image if no suitable contour was found
-        return image
+    print(displayCnt)
+    img1 = image
+    cv2.drawContours(img1, [displayCnt], -1, (0,255,0), 2)
+    cv2.imshow("result", img1)
+    cv2.waitKey(0)
+# Obtain birds' eye view of image
+    warped = four_point_transform(image, displayCnt.reshape(4, 2))
+
+    return warped
+
