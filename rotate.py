3c82d6fa04061f8ec164ad657e1011bf84e074e8
hieuoneplus <nshd183@gmail.com>
2023-12-13 14:09:11 +0700

    add

--- rotate.py
+++ rotate.py
@@ -6,9 +6,9 @@ def rotate(image):
     # Load image, grayscale, Gaussian blur, Otsu's threshold
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (9,9), 0)
-    thresh = cv2.adaptiveThreshold(
-        blur, 220, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 3)
-
+    # thresh = cv2.adaptiveThreshold(
+    #     blur, 220, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 3)
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
     # Find contours and sort for largest contour
     cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
