93e9c629cd8b8628a3edf791a49d121f84646ef3
hieuoneplus <nshd183@gmail.com>
2023-12-16 10:29:18 +0700

    add rotate

--- rotate.py
+++ rotate.py
@@ -5,11 +5,19 @@ import numpy as np
 def rotate(image):
     # Load image, grayscale, Gaussian blur, Otsu's threshold
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
+    # warpedd = cv2.resize(gray, (800, 600))
+    # cv2.imshow("gray", warpedd)
+    # cv2.waitKey()
     blur = cv2.GaussianBlur(gray, (9,9), 0)
-
+    # warpedd = cv2.resize(blur, (800, 600))
+    # cv2.imshow("blur", warpedd)
+    # cv2.waitKey()
+    # thresh = cv2.adaptiveThreshold(
+    #     blur, 200, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 3)
     thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-
+    # warpedd = cv2.resize(thresh, (800, 600))
+    # cv2.imshow("thres", warpedd)
+    # cv2.waitKey()
     # Find contours and sort for largest contour
     cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
@@ -27,9 +35,16 @@ def rotate(image):
     if displayCnt is not None:
         # Obtain birds' eye view of image
         warped = four_point_transform(image, displayCnt.reshape(4, 2))
+        # warpedd = cv2.resize(warped, (800, 600))
+        # cv2.imshow("rotate", warpedd)
+        # cv2.waitKey()
         return warped
     else:
         # Return original image if no suitable contour was found
         return image
 
-
+# image_path = cv2.imread("5.JPG")
+# warped = rotate(image_path)
+# warped = cv2.resize(warped, (800, 600))
+# cv2.imshow("warped", warped)
+# cv2.waitKey()
